4564b852d297d66a283351717ec5bfcd43890ab8
d0t-J <j.ahmed.ja7a7@gmail.com>
2025-12-09 20:27:25 +0500

    added calculation for zero_crossing count

--- 25/1. Secret Entrance/1.py
+++ 25/1. Secret Entrance/1.py
@@ -11,4 +11,7 @@ with open("input.txt", "r") as f:
             val = (val + num) % 100
         elif direction == "L":
             val = (val - num + 100) % 100
+
+        if val == 0:
+            zero_crossing += 1
 print(zero_crossing)
